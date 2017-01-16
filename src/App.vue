4b4a486b02f1cc611764d58bac308b90d1c4a41d
mathieu-anderson <mathieu.hj.anderson@gmail.com>
2017-01-17 07:12:23 +1300

    Add bits of code for routes and auth0

--- src/App.vue
+++ src/App.vue
@@ -1,4 +1,20 @@
 <template>
+
+  <!-- <nav class="navbar navbar-default">
+     <div class="container">
+       <ul class="nav navbar-nav">
+         <li><a v-link="'home'">Home</a></li>
+         <li><a v-link="'login'">Login</a></li>
+         <li><a v-link="'signup'">Sign Up</a></li>
+         <li><a v-link="'secretquote'">Secret Quote</a></li>
+         <li><a v-link="'login'">Logout</a></li>
+       </ul>
+     </div>
+   </nav>
+   <div class="container">
+     <router-view></router-view>
+   </div> -->
+
   <div id="app">
     <h1>tasteFight</h1>
       <h2>{{msg}}</h2>
--- src/main.js
+++ src/main.js
@@ -1,6 +1,34 @@
 import Vue from 'vue'
 import App from './App.vue'
 
+// import VueRouter from 'vue-router'
+// import VueResource from 'vue-resource'
+// Vue.use(VueRouter)
+// Vue.use(VueResource)
+//
+// export const router = new VueRouter()
+//
+// router.map({
+//   '/home': {
+//     component: App
+//   },
+//   '/login': {
+//     component: App
+//   },
+//   '/signup': {
+//     component: App
+//   },
+//   '/about': {
+//     component: App
+//   }
+// })
+//
+// router.redirect({
+//   '*': '/home'
+// })
+//
+// router.start(App, '#app')
+
 new Vue({
   el: '#app',
   render: h => h(App)
