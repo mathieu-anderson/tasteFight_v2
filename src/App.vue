2de87ce6648010be170599164377b3b278e08065
mathieu-anderson <mathieu.hj.anderson@gmail.com>
2017-01-18 14:17:12 +1300

    Add API calls to button

--- src/App.vue
+++ src/App.vue
@@ -18,28 +18,60 @@
   <div id="app">
     <h1>tasteFight</h1>
       <h2>{{msg}}</h2>
-      <form v-show="showForm" v-on:submit.prevent="onSubmit" class="submitMovie">
-        <input type="text" name="" value="" v-model="movie_name">
-        <input type="submit" name="submit" id="submit" value="fight!" v-on:keyup.enter="submit">
+      <form
+        v-show="showForm"
+        v-on:submit.prevent="onSubmit">
+          <input
+          type="text"
+          v-show="showForm"
+          v-model="movie_name">
+          <input
+          type="submit"
+          value="fight!"
+          v-show="showForm"
+          v-on:keyup.enter="submit">
       </form>
-      <div id="movie_name">{{movie_name}}</div>
+      <div id="movie_name"
+        v-show="showForm">
+          {{movie_name}}
+      </div>
+      <div class="api_res">
+        <b>{{api_res_name}}</b>
+        {{api_res_overview}}
+        <!-- <img v-bind:src="https://image.tmdb.org/t/p/w300_and_h450_bestv2/{{api_res_poster}}"> -->
+      </div>
   </div>
 </template>
 
 <script>
+// import VueInstant from 'vue-instant'
+// import 'vue-instant/dist/vue-instant.css'
+// Vue.use(VueInstant)
+import axios from 'axios'
+// const TMDB_api_key =
+
 export default {
   name: 'app',
   data () {
     return {
       msg: 'Fight for your tastes!',
       showForm: true,
-      movie_name: ''
+      movie_name: '',
+      api_res_name: '',
+      api_res_overview: '',
+      image_url: 'https://image.tmdb.org/t/p/w300_and_h450_bestv2/'
     }
   },
   methods: {
     onSubmit: function () {
       console.log(this.movie_name)
-      return this.showForm = false
+      axios.get(`https://api.themoviedb.org/3/search/movie?api_key=3afb334973093028cc5d28d0464b6383&language=en-US&query=${this.movie_name}&page=1&include_adult=false`)
+        .then(res => {
+          this.api_res_name = res.data.results[0].original_title
+          this.api_res_overview = res.data.results[0].overview
+          this.api_res_poster = res.data.results[0].poster_path
+        })
+      this.showForm = false
     }
   }
 }
@@ -59,10 +91,6 @@ export default {
   padding: 20px;
   font-size: 60px;
   font-style: italic;
-}
-
-.submitMovie {
-  padding: 20px;
 }
 
 h1 {
